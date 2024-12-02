952f8819e68a974439cc5fddee46e7fb4baa0153
Siempouwels <108520193+Siempouwels@users.noreply.github.com>
2024-12-02 20:48:23 +0100

    Comparable added and change dyn arr

--- adp/DynamicArrayImplementation.cs
+++ adp/DynamicArrayImplementation.cs
@@ -2,53 +2,41 @@ namespace adp;
 
 public class DynamicArrayImplementation<T>
 {
-    private T[] _array = [];
+    private T[] _array = new T[2];
     private int _size;
-    
+
     public void Add(T element)
     {
-        if (_array.Length == _size)
-        {
-            var newArray = new T[_size + 1];
-            for (int i = 0; i < _size; i++)
-            {
-                newArray[i] = _array[i];
-            }
-            _array = newArray;
-        }
+        EnsureCapacity();
         _array[_size++] = element;
     }
-    
+
     public T Get(int index)
     {
+        ValidateIndex(index);
         return _array[index];
     }
-    
+
     public void Set(int index, T element)
     {
+        ValidateIndex(index);
         _array[index] = element;
     }
 
     public void Remove(int index)
     {
+        ValidateIndex(index);
+
         for (int i = index; i < _size - 1; i++)
         {
             _array[i] = _array[i + 1];
         }
-        
+
         _size--;
-        
-        if (_size < _array.Length)
-        {
-            var newArray = new T[_size];
-            for (var i = 0; i < _size; i++)
-            {
-                newArray[i] = _array[i];
-            }
-            _array = newArray;
-        }
+        _array[_size] = default!;
+        ShrinkCapacityIfNeeded();
     }
-    
+
     public bool Contains(T element)
     {
         for (int i = 0; i < _size; i++)
@@ -58,18 +46,57 @@ public class DynamicArrayImplementation<T>
                 return true;
             }
         }
+
         return false;
     }
-    
+
     public int IndexOf(T element)
     {
-        for (var i = 0; i < _size; i++)
+        for (int i = 0; i < _size; i++)
         {
-            if (_array[i]!.Equals(element))
+            if (_array[i]!.AreEqual(element))
             {
                 return i;
             }
         }
+
         return -1;
+    }
+
+    private void EnsureCapacity()
+    {
+        if (_size >= _array.Length)
+        {
+            ResizeArray(_array.Length * 2);
+        }
+    }
+
+    private void ShrinkCapacityIfNeeded()
+    {
+        var newCapacity = _array.Length * 2;
+
+        if (_size > 0 && _size <= newCapacity)
+        {
+            ResizeArray(newCapacity);
+        }
+    }
+
+    private void ResizeArray(int newCapacity)
+    {
+        var newArray = new T[newCapacity];
+        for (int i = 0; i < _size; i++)
+        {
+            newArray[i] = _array[i];
+        }
+
+        _array = newArray;
+    }
+
+    private void ValidateIndex(int index)
+    {
+        if (index < 0 || index >= _size)
+        {
+            throw new IndexOutOfRangeException($"Index {index} is out of range. Size: {_size}");
+        }
     }
 }
---
+++ adp/DynamicComparable.cs
@@ -0,0 +1,29 @@
+﻿namespace adp;
+
+public static class DynamicComparable
+{
+    public static bool AreEqual<T>(this T obj1, T obj2)
+    {
+        if (obj1 == null && obj2 == null) return true;
+        if (obj1 == null || obj2 == null) return false;
+
+        if (typeof(T).IsPrimitive || typeof(T).IsValueType || typeof(T) == typeof(string))
+        {
+            return obj1.Equals(obj2);
+        }
+
+        var properties = typeof(T).GetProperties();
+        foreach (var property in properties)
+        {
+            var value1 = property.GetValue(obj1);
+            var value2 = property.GetValue(obj2);
+
+            if (!Equals(value1, value2))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
