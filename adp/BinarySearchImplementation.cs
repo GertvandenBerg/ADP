f70e592ae0b1ab0d809c3e6954052bc7629b285b
Siempouwels <108520193+Siempouwels@users.noreply.github.com>
2024-12-04 22:51:05 +0100

    Finished BinarySearchImplementation.cs

---
+++ adp/BinarySearchImplementation.cs
@@ -0,0 +1,22 @@
+﻿namespace adp;
+
+public class BinarySearchImplementation
+{
+    public int BinarySearch<T>(T[] sortedArray, T target, Func<T, T, int> customComparer)
+    {
+        int left = 0;
+        int right = sortedArray.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            int comparison = customComparer(sortedArray[mid], target);
+            if (comparison == 0) return mid;
+            if (comparison > 0) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return -1;
+    }
+}
