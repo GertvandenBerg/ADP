57da6183202c7c67e92c8ce37323ee0c20904659
Siempouwels <108520193+Siempouwels@users.noreply.github.com>
2024-12-04 22:51:25 +0100

    Finished InsertionSortImplementation.cs

---
+++ adp/InsertionSortImplementation.cs
@@ -0,0 +1,21 @@
+﻿namespace adp;
+
+public class InsertionSortImplementation
+{
+    public void InsertionSort<T>(T[] array, Func<T, T, int> customComparer)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            T key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && customComparer(array[j], key) > 0)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+}
